5b7909e827325f6c28e3f9e92d38041bf25df5a3
Anthony Viveros <anjoviveros123@umariana.edu.co>
2023-10-12 22:20:11 -0500

    se agrego codigo

---
+++ Trabajo.java
@@ -0,0 +1,24 @@
+package Clases;
+
+import javax.swing.JOptionPane;
+
+public class Ejer1 {
+
+    public static void main(String[] args) {
+        // Uso de contenedoras
+        // Constantes
+        double notas[] = new double[3];
+        double promedio;
+        try {
+            notas[0] = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota 1"));
+            notas[1] = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota 2"));
+            notas[2] = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota 3"));
+            promedio = (notas[0] + notas[1] + notas[2]) / 3;
+            JOptionPane.showMessageDialog(null, "Las notas son " + notas[0] + "  " + notas[1] + "  " + notas[2]);
+            JOptionPane.showMessageDialog(null, "el promedio es " + promedio);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error: Ingrese un valor numérico válido.");
+        }
+
+    }
+}
